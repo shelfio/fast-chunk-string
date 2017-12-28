d462b46bef834485cee8cb0acc085f441b0c2c05
Vlad Holubiev <golubev.vld@gmail.com>
2017-12-28 17:06:27 +0200

    chore: add prettier

--- index.test.js
+++ index.test.js
@@ -7,9 +7,9 @@ it('should export a module', () => {
 });
 
 it('should split string into chunks of even number  of chars', () => {
-  expect(m(text, 4)).toEqual(["hell", "o my", " dea", "r wo", "rld"]);
+  expect(m(text, 4)).toEqual(['hell', 'o my', ' dea', 'r wo', 'rld']);
 });
 
 it('should split string into chunks of odd number  of chars', () => {
-  expect(m(text, 3)).toEqual(["hel", "lo ", "my ", "dea", "r w", "orl", "d"]);
+  expect(m(text, 3)).toEqual(['hel', 'lo ', 'my ', 'dea', 'r w', 'orl', 'd']);
 });
