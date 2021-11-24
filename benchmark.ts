0b804982cd48a690bed6644523340f2ab8872f99
Vlad Holubiev <golubev.vld@gmail.com>
2021-11-24 11:10:08 +0200

    feat: switch from benchmark.js to benny

--- benchmark.ts
+++ benchmark.ts
@@ -1,51 +1,56 @@
-import Benchmark from 'benchmark';
+import benny from 'benny';
 import fLI from 'fast-lorem-ipsum';
 import fastChunkString from './lib';
 
-const suite = new Benchmark.Suite();
 const words5000 = fLI(5000, 'w');
 const words50000 = fLI(50000, 'w');
 const words500000 = fLI(500000, 'w');
 const words5000000 = fLI(5000000, 'w');
 
-suite
-  .add('~33 kb split by 2 kb', () => {
+benny.suite(
+  'Without Unicode',
+  benny.add('~33 kb split by 2 kb', () => {
     fastChunkString(words5000, {size: 2 * 1024});
-  })
-  .add('~33 kb split by 1 mb', () => {
+  }),
+  benny.add('~33 kb split by 1 mb', () => {
     fastChunkString(words5000, {size: 1024 * 1024});
-  })
-  .add('~330 kb split by 2 kb', () => {
+  }),
+  benny.add('~330 kb split by 2 kb', () => {
     fastChunkString(words50000, {size: 2 * 1024});
-  })
-  .add('~330 kb split by 1 mb', () => {
+  }),
+  benny.add('~330 kb split by 1 mb', () => {
     fastChunkString(words50000, {size: 1024 * 1024});
-  })
-  .add('~3.3 mb split by 2 kb', () => {
+  }),
+  benny.add('~3.3 mb split by 2 kb', () => {
     fastChunkString(words500000, {size: 2 * 1024});
-  })
-  .add('~3.3 mb split by 1 mb', () => {
+  }),
+  benny.add('~3.3 mb split by 1 mb', () => {
     fastChunkString(words500000, {size: 1024 * 1024});
-  })
-  .add('~33 mb split by 2 kb', () => {
+  }),
+  benny.add('~33 mb split by 2 kb', () => {
     fastChunkString(words5000000, {size: 2 * 1024});
-  })
-  .add('~33 mb split by 1 mb', () => {
+  }),
+  benny.add('~33 mb split by 1 mb', () => {
     fastChunkString(words5000000, {size: 1024 * 1024});
-  })
-  .add('~33 kb split by 2 kb with unicodeAware', () => {
+  }),
+  benny.cycle(),
+  benny.complete()
+);
+
+benny.suite(
+  'Unicode Aware',
+  benny.add('~33 kb split by 2 kb with unicodeAware', () => {
     fastChunkString(words5000, {size: 2 * 1024, unicodeAware: true});
-  })
-  .add('~33 kb split by 1 mb with unicodeAware', () => {
+  }),
+  benny.add('~33 kb split by 1 mb with unicodeAware', () => {
     fastChunkString(words5000, {size: 1024 * 1024, unicodeAware: true});
-  })
-  .add('~330 kb split by 2 kb with unicodeAware', () => {
+  }),
+  benny.add('~330 kb split by 2 kb with unicodeAware', () => {
     fastChunkString(words50000, {size: 2 * 1024, unicodeAware: true});
-  })
-  .add('~330 kb split by 1 mb with unicodeAware', () => {
+  }),
+  benny.add('~330 kb split by 1 mb with unicodeAware', () => {
     fastChunkString(words50000, {size: 1024 * 1024, unicodeAware: true});
-  })
-  .on('cycle', function (event) {
-    console.log(String(event.target));
-  })
-  .run({async: true});
+  }),
+  benny.cycle(),
+  benny.complete()
+);
--- readme.md
+++ readme.md
@@ -30,21 +30,57 @@ fastChunkString('😀😃😄😁', {size: 2, unicodeAware: true});
 
 ## Benchmarks
 
-Run via `yarn benchmark`. Measured on Macbook Pro 13" with M1 processor.
+Run via `yarn benchmark`. Measured on Macbook Pro 16" with M1 Max processor.
 
 ```
-~33 kb split by 2 kb x 8,902,281 ops/sec ±0.37% (97 runs sampled)
-~33 kb split by 1 mb x 100,872,434 ops/sec ±0.65% (92 runs sampled)
-~330 kb split by 2 kb x 1,009,220 ops/sec ±0.89% (95 runs sampled)
-~330 kb split by 1 mb x 97,087,201 ops/sec ±5.51% (90 runs sampled)
-~3.3 mb split by 2 kb x 102,498 ops/sec ±1.74% (97 runs sampled)
-~3.3 mb split by 1 mb x 33,719,853 ops/sec ±2.55% (94 runs sampled)
-~33 mb split by 2 kb x 9,953 ops/sec ±1.21% (95 runs sampled)
-~33 mb split by 1 mb x 4,976,079 ops/sec ±1.22% (94 runs sampled)
-~33 kb split by 2 kb with unicodeAware x 37.98 ops/sec ±0.16% (65 runs sampled)
-~33 kb split by 1 mb with unicodeAware x 355 ops/sec ±0.29% (89 runs sampled)
-~330 kb split by 2 kb with unicodeAware x 0.40 ops/sec ±1.19% (5 runs sampled)
-~330 kb split by 1 mb with unicodeAware x 34.99 ops/sec ±0.91% (61 runs sampled)
+Running "Without Unicode" suite...
+Progress: 100%
+
+  ~33 kb split by 2 kb:
+    14 384 027 ops/s, ±0.21%    | 86.51% slower
+
+  ~33 kb split by 1 mb:
+    106 655 332 ops/s, ±0.12%   | fastest
+
+  ~330 kb split by 2 kb:
+    1 482 220 ops/s, ±0.34%     | 98.61% slower
+
+  ~330 kb split by 1 mb:
+    106 442 883 ops/s, ±0.14%   | 0.2% slower
+
+  ~3.3 mb split by 2 kb:
+    153 356 ops/s, ±0.19%       | 99.86% slower
+
+  ~3.3 mb split by 1 mb:
+    48 824 019 ops/s, ±0.16%    | 54.22% slower
+
+  ~33 mb split by 2 kb:
+    14 312 ops/s, ±0.28%        | slowest, 99.99% slower
+
+  ~33 mb split by 1 mb:
+    7 059 008 ops/s, ±0.32%     | 93.38% slower
+
+Finished 8 cases!
+  Fastest: ~33 kb split by 1 mb
+  Slowest: ~33 mb split by 2 kb
+Running "Unicode Aware" suite...
+Progress: 100%
+
+  ~33 kb split by 2 kb with unicodeAware:
+    450 ops/s, ±0.24%   | fastest
+
+  ~33 kb split by 1 mb with unicodeAware:
+    428 ops/s, ±0.36%   | 4.89% slower
+
+  ~330 kb split by 2 kb with unicodeAware:
+    45 ops/s, ±0.38%    | 90% slower
+
+  ~330 kb split by 1 mb with unicodeAware:
+    41 ops/s, ±0.42%    | slowest, 90.89% slower
+
+Finished 4 cases!
+  Fastest: ~33 kb split by 2 kb with unicodeAware
+  Slowest: ~330 kb split by 1 mb with unicodeAware
 ```
 
 ## Publish
